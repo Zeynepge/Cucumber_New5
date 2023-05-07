85d8e41211f423b0207ac829e117b0de5ee15ac2
Zeynepge <sgunal571@gmail.com>
2023-05-07 16:28:29 +0200

    edit2

--- src/test/java/Runners/_04_TestRunnerSmoke.java
+++ src/test/java/Runners/_04_TestRunnerSmoke.java
@@ -9,6 +9,8 @@ import io.cucumber.testng.CucumberOptions;
         glue = {"StepDefinitions"},
         plugin= {"html:target/site/cucumber-pretty.html"}
         // plugin normalde yok, jenkins icin ekliyoruz
+
+
 )
 public class _04_TestRunnerSmoke extends AbstractTestNGCucumberTests {
 }
