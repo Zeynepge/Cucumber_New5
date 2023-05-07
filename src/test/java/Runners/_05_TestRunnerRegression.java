48ccfe0a835fae32d8fb83a2e7815145e22aecf0
Zeynepge <sgunal571@gmail.com>
2023-05-07 22:31:27 +0200

    jenkins ayarlari

--- src/test/java/Runners/_05_TestRunnerRegression.java
+++ src/test/java/Runners/_05_TestRunnerRegression.java
@@ -7,7 +7,8 @@ import io.cucumber.testng.CucumberOptions;
         tags = "@Regression or @SmokeTest", // hangi senaryolarda bu etiket varsa onlar çalıştırılacak
         // or yerine and yazılırsa hem smoke hem de regression tag i olanları çalıştırıyor
         features = {"src/test/java/FeatureFiles"},
-        glue = {"StepDefinitions"}
+        glue = {"StepDefinitions"},
+        plugin = {"json:target/cucumber/cucumber.json"} // Jenkins için JSON report
 )
 public class _05_TestRunnerRegression extends AbstractTestNGCucumberTests {
 }
